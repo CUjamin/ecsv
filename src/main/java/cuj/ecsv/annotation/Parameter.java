135955fbe979c4e71379eb0f12b8a71da5a1ead6
cujamin <chbnjamin@gmail.com>
2020-03-21 23:01:42 +0800

    增加注解功能

---
+++ src/main/java/cuj/ecsv/annotation/Parameter.java
@@ -0,0 +1,17 @@
+package cuj.ecsv.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author cujamin
+ * @date 2020/3/21
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER,ElementType.METHOD,ElementType.FIELD})
+public @interface Parameter {
+    String name();
+}
